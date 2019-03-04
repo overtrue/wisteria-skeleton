e2824da9c32adc9292b75411a4cc86d7993c6412
overtrue <anzhengchao@gmail.com>
2019-03-04 15:36:25 +0800

    bugfix

---
+++ config/wisteria.php
@@ -0,0 +1,57 @@
+<?php
+
+return [
+    'route' => '/docs',
+
+    'docs' => [
+        // Storage path of docs files.
+        'path' => '/resources/docs',
+
+        // Index page file.
+        'index' => 'index.md',
+
+        // Homepage
+        'home' => 'overview.md',
+
+        // Versions
+        'versions' => [
+            'master', '3.x', '4.0',
+        ],
+        'default_version' => 'master',
+
+        // Docs repository
+        'repository'   => [
+            'provider' => 'github',
+            'url'      => 'https://github.com/easywechat/docs',
+        ],
+    ],
+
+    // SEO configs
+    'seo'                 => [
+        'author'          => 'Wisteria',
+        'description'     => '',
+        'keywords'        => '',
+        'og'              => [
+            'title'       => '',
+            'type'        => 'article',
+            'url'         => '',
+            'image'       => '',
+            'description' => '',
+        ]
+    ],
+
+    'plugins' => [
+        'google-analytics' => [
+            // 'id' => 'UA-XXXXXXXX-1'
+        ],
+        'docsearch' => [
+            'api_key' => '25626fae796133dc1e734c6bcaaeac3c',
+            'index_name' => 'docsearch',
+            'placeholder' => 'Search docs',
+        ]
+    ],
+
+    'cache' => [
+        'ttl' => 0, // seconds
+    ],
+];
